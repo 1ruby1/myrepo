346ae3f6c56db4646c8bf7a10342a90464f7e578
ruby go <ruby go>
2016-03-14 22:30:32 +0900

    what second MSG...

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 
   public static void main(String[] args) {
-
     System.out.println ("Hello World-----");
+    System.out.println ("line num 2...");
+    System.out.println ("line num 3...");
   }
 }
